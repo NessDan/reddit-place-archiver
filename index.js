b674bc4858cd73da48c9b1d112b001d93a914b47
Daniel Kaspo <dkaspo@barracuda.com>
2017-03-31 16:26:56 -0400

    Added a variable for people running to add the key that reddit assigns on visit.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const WebSocket = require('ws');
 const fs = require('fs');
+const secretSauce = 'PutTheTokenHere';
 
 if (!fs.existsSync('paintings')) {
 	fs.mkdirSync('paintings');
 }
 
 const outputStream = fs.createWriteStream('paintings/' + (new Date).getTime() + '.txt');
-const ws = new WebSocket('wss://ws-064f7ddfdc34212b7.wss.redditmedia.com/place?m=AQAAjcXeWO3sQdw7yfQwuua7JOK9ZySjEw4bsDwvaxX2yF79jYni');
+const ws = new WebSocket('wss://ws-064f7ddfdc34212b7.wss.redditmedia.com/place?m=' + secretSauce);
 
 ws.on('message', (data, flags) => {
 	outputStream.write(data);
